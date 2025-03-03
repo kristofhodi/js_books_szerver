1de07c9b3c429ddbdc4868eccaaf090c672176a1
kristofhodi <144095755+kristofhodi@users.noreply.github.com>
2025-03-03 13:30:24 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const books = [
+    {author: "Orosz György", title: "Feri naplója", year: 2022},
+    {author: "Dan Brown", title: "Angyalok és démonok", year: 2017},
+    {author: "Gárdonyi Géza", title: "Egri csillagok", year: 1901},
+]
+
+export default books
